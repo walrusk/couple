26e9694ed153017dd3a40b49b17b0dc32a713b9d
Rob N <walrusk@gmail.com>
2022-02-27 17:25:39 -0600

    icon space

--- src/GameOver.js
+++ src/GameOver.js
@@ -41,7 +41,7 @@ function GameOver({ game, className, showStats, setShowStats }) {
             <Share game={game} />
             <button className={`btn btn-sm ${showStats && 'btn-active'}`} onClick={() => setShowStats(!showStats)}>
               stats
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
               </svg>
             </button>
