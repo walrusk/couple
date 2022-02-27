45335f55f9558343c6aa73d4893ba47f2a42f08b
Rob N <walrusk@gmail.com>
2022-02-27 17:36:51 -0600

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import Game from './Game';
 import GameMode from './GameMode';
 import Stats from './Stats';
@@ -8,12 +8,13 @@ import SlideToggle from './SlideToggle';
 function App() {
   const game = useGameState();
   const [showStats,setShowStats] = useState(false);
+  useEffect(updateTheme, []);
   return (
     <div className="App">
       <header className="text-center p-4 bg-base-300">
         <div className="flex items-center justify-center space-x-6">
           <div className="prose">
-            <h1 className="m-0"><a href="/">Couple</a></h1>
+            <h1 className="m-0"><a href="/" onClick={toggleTheme}>Couple</a></h1>
           </div>
           <GameMode game={game} showStats={showStats} setShowStats={setShowStats} />
         </div>
@@ -31,6 +32,28 @@ function App() {
       </div>
     </div>
   );
+}
+
+function updateTheme() {
+  const theme = window.localStorage.getItem('theme');
+  if (theme !== null) {
+    setTheme(theme);
+  }
+}
+
+function toggleTheme() {
+  const systemDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const theme = window.localStorage.getItem('theme');
+  if (theme === null) {
+    setTheme(systemDark ? 'light' : 'dark');
+  } else {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  }
+}
+
+function setTheme(theme) {
+  window.localStorage.setItem('theme', theme);
+  document.documentElement.setAttribute('data-theme', theme);
 }
 
 export default App;
