a2973ff65d37a0042a2a29a333727ae5b3635bc5
Rob N <walrusk@gmail.com>
2022-02-27 22:55:41 -0600

    add daily

--- src/Stats.js
+++ src/Stats.js
@@ -8,7 +8,7 @@ function Stats({ game, setShowStats }) {
     <div className="py-4">
       <div className="pt-2 pb-4 text-center relative bg-base-200 rounded-xl">
         <h3 className="mt-2">
-          {practice ? 'Practice ' : ''} Stats
+          {practice ? 'Practice ' : 'Daily'} Stats
           <button className="btn btn-ghost btn-circle btn-sm opacity-50 absolute right-2" onClick={() => setShowStats(false)}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
